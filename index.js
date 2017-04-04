4403b3854da554e3a264f0133ac56ac43d6cd6d2
huangxutao <h784124804@gmail.com>
2017-04-04 17:58:43 +0800

    唯一IP

--- index.js
+++ index.js
@@ -6,21 +6,35 @@ const Io = require('socket.io')(Server);
  * 所有在线 chatter - 暂时用数组 不知是必要数据库
  */
 let chatter = {
+  name: [],
   storage: [],
 
-  add: function(chatter) {
-    return this.storage.push(chatter);
+  add: function(name, chatter) {
+    this.name.push(name);
+    this.storage.push(chatter);
   },
   
-  remove: function(chatter) {
-    let index = this.storage.indexOf(chatter);
+  remove: function(name) {
+    let index = this.name.indexOf(name);
 
-    return this.storage.splice(index, 1);
+    this.name.splice(index, 1);
+    this.storage.splice(index, 1);
+  },
+
+  get: function (name) {
+    return this.storage[this.name.indexOf(name)];
   }
-}
+};
 
 Io.of('/group-chat').on('connection', function (socket) {
   let reqIP = socket.handshake.headers['x-real-ip'] || socket.handshake.address.substr(7);
+
+  // 唯一 IP 原则
+  if(chatter.name.indexOf(reqIP) >= 0) {
+
+    return socket.emit('refused', `您的IP: ${reqIP} 已被占用...`);
+  }
+
   let reqDate = new Date();
   let thisChatter = {
     name: reqIP,
@@ -36,23 +50,25 @@ Io.of('/group-chat').on('connection', function (socket) {
     content: `🙋 ${reqIP} 加入群聊 ${reqDate.toString().split(' ')[4]}`
   };
 
+
   /**
    * new chatter
    */
-  chatter.add(thisChatter);
+  chatter.add(reqIP, thisChatter);
   socket.emit('join', thisChatter);
   socket.broadcast.emit('s-msg', msg);
-  
+    
   // 取消引用，方便下次直接垃圾回收时 ？
   reqDate = null;
   msg = null;
+  thisChatter = null;
 
   /**
    * Response 聊天消息
    */
   socket.on('c-msg', function (cMsg) {
     let msg = {
-      from: thisChatter,
+      from: chatter.get(reqIP),
       to: cMsg.to,
       type: cMsg.type,
       date: new Date,
